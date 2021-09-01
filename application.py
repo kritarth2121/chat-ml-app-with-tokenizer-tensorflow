b6abc45bc4a16c92d1a7337e5090ba89c01e2f18
Kritarth Sharma <kritarth2121@gmail.com>
2021-09-01 22:02:28 +0530

    Initial commit

---
+++ application.py
@@ -0,0 +1,37 @@
+from chatbot import model,labels,bag_of_words,words,data
+import sys
+# if sys.version_info.major < 3:
+#     reload(sys)
+# sys.setdefaultencoding('utf8')
+from flask import Flask, render_template, request
+import numpy as np
+import random
+from flask_cors import CORS
+
+application = Flask(__name__, static_url_path='/static')
+
+CORS(application)
+application.config['CORS_HEADERS'] = 'Content-Type'
+
+application.static_folder = 'static'
+
+@application.route("/")
+def home():
+    return render_template("index.html")
+
+@application.route("/get")
+def get_bot_response():
+    inp = request.args.get('msg')
+    results = model.predict([bag_of_words(inp, words)])
+    results_index = np.argmax(results)
+    tag = labels[results_index]
+
+    for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+
+    return (random.choice(responses))
+
+
+if __name__ == "__main__":
+    application.run()
