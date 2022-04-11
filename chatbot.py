aba80e081ba8094e65439fe727080a58a2f5246c
kritarth <kritarth2121@gmail.com>
2022-04-11 20:31:06 +0530

    clean code

--- chatbot.py
+++ chatbot.py
@@ -117,7 +117,8 @@ net = tflearn.regression(net)
 model = tflearn.DNN(net)
 
 try:
-    model.load("model.tflearn")
+    with open("model.tflearn", "rb") as f:
+        model.load("model.tflearn")
 except:
     model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
     model.save("model.tflearn")
