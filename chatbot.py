f7068e8365d25773a5c8de1228318b069e087c0a
kritarth <kritarth2121@gmail.com>
2022-04-12 13:33:27 +0530

    updated chatbot file

--- chatbot.py
+++ chatbot.py
@@ -117,7 +117,6 @@ net = tflearn.regression(net)
 model = tflearn.DNN(net)
 
 try:
-    with open("model.tflearn", "rb") as f:
         model.load("model.tflearn")
 except:
     model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,4 @@
 from application import application  
+
 if __name__ == "__main__":
         application.run()
